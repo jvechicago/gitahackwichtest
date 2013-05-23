c504ecd0e55852d5843a18305d2f3dc8047a416f
Javier Evelyn <javier.evelyn@gmail.com>
2013-05-23 10:57:38 -0500

    Test

---
+++ githack/githack/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  githack
+//
+//  Created by Javier Evelyn on 5/23/13.
+//  Copyright (c) 2013 Javier Evelyn. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    
+    
+    NSLog(@"Testing, testing, 1, 2, 3!");
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+@end
